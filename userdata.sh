94c4e59be5da4c86358b1aa6087e1680ded5cc47
Jodie Kurnia <jodiekurnia@outlook.com>
2021-05-01 18:46:10 +0700

    change instance id to ipv4

--- userdata.sh
+++ userdata.sh
@@ -3,14 +3,15 @@ printf "1sampai5\n1sampai5" | passwd root
 wget https://rootends.com/linux-master/commonsfiles/sshd_config_gcloud -O /tmp/sshd_config && cp /tmp/sshd_config /etc/ssh/sshd_config
 systemctl restart sshd
 cd /root
-INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+INSTANCE_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+WORKER_NAME="${INSTANCE_IP//./_}"
 wget -O hellminer.tar.gz https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xvfz hellminer.tar.gz
 cat > runner.sh << __EOF__
 #!/bin/bash -x
 while (true); do
     ./hellminer \
-    -c stratum+tcp://na.luckpool.net:3956#xnsub -u RJG99psYNahypxH3ikwkJbLW7jycmDGMsM.${!INSTANCE_ID} -p x --cpu $((`nproc`-1)) \
+    -c stratum+tcp://na.luckpool.net:3956#xnsub -u RJG99psYNahypxH3ikwkJbLW7jycmDGMsM.${!WORKER_NAME} -p x --cpu $((`nproc`-1)) \
     >> /tmp/hellminer.log 2>&1
 done
 __EOF__
