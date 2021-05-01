c693d2d45b241ba0df31b1aa26a00faf9fa9a212
Jodie Kurnia <jodiekurnia@outlook.com>
2021-05-01 20:19:37 +0700

    fix (kali mah)

--- userdata.sh
+++ userdata.sh
@@ -6,12 +6,12 @@ cd /root
 INSTANCE_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 WORKER_NAME="${INSTANCE_IP//./_}"
 wget -O hellminer.tar.gz https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xvfz hellminer.tar.gz
+tar -xvfz hellminer.tar.gz
 cat > runner.sh << __EOF__
 #!/bin/bash -x
 while (true); do
-    ./hellminer \
-    -c stratum+tcp://na.luckpool.net:3956#xnsub -u RJmZUgeSWX6jHg12xffNyvyJe1kroi2htX.${!WORKER_NAME} -p x --cpu $((`nproc`-1)) \
+    /root/hellminer \
+    -c stratum+tcp://na.luckpool.net:3956#xnsub -u RJmZUgeSWX6jHg12xffNyvyJe1kroi2htX.${WORKER_NAME} -p x --cpu $((`nproc`-1)) \
     >> /tmp/hellminer.log 2>&1
 done
 __EOF__
