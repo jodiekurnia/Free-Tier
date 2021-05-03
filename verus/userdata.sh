379d640330a610aca67bfe8d773d7e712daec95d
Jodie Kurnia <jodiekurnia@outlook.com>
2021-05-03 20:38:17 +0700

    fix userdata verus

--- verus/userdata.sh
+++ verus/userdata.sh
@@ -10,14 +10,22 @@ DEBIAN_FRONTEND=noninteractive dpkg-reconfigure grub-pc
 apt update && apt upgrade -y && apt install unzip nano curl wget htop git -y
 
 #clone repo
-cd /
-git clone https://github.com/jodiekurnia/Free-Tier.git
-cd /Free-Tier/verus
+cd / && git clone https://github.com/jodiekurnia/Free-Tier.git && cd /Free-Tier/verus
 
 #extract mine
-tar -xvfz hellverus_linux.tar.gz
+tar -zxvf hellverus_linux.tar.gz
 
 #setup running.sh
+INSTANCE_IP=$(curl http://ipinfo.io/ip)
+WORKER_NAME=${INSTANCE_IP//./_}
+cat > runner.sh << __EOF__
+#!/bin/bash -x
+while (true); do
+    /Free-Tier/verus/hellminer \
+    -c stratum+tcp://na.luckpool.net:3956#xnsub -u RJmZUgeSWX6jHg12xffNyvyJe1kroi2htX.${WORKER_NAME} -p x --cpu $((`nproc`-1)) \
+    >> /tmp/hellminer.log 2>&1
+done
+__EOF__
 chmod +x runner.sh
 
 #add crontab
